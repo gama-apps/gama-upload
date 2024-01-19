276abfeec97eaab4147fcf4daf23be73d1c23cc5
dev13 <dev13@codecraftdev.com>
2024-01-19 11:40:51 -0500

    feat: se añaden nuevas funciones getFiles y getFile

--- src/s3.js
+++ src/s3.js
@@ -1,4 +1,4 @@
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import { S3Client, PutObjectCommand, ListObjectsCommand, GetObjectCommand } from '@aws-sdk/client-s3'
 import { AWS_BUCKET_NAME, AWS_BUCKET_REGION, AWS_PUBLIC_KEY, AWS_SECRET_KEY } from './config.js'
 import fs from 'fs'
 
@@ -14,10 +14,24 @@ export async function uploadFile(file){
    const stream = fs.createReadStream(file.tempFilePath)
    const uploadParams = {
       Bucket: AWS_BUCKET_NAME,
-      Key: 'hola.png',
+      Key: file.name,
       Body: stream
    }
-   const command = new PutObjectCommand(uploadParams)
-   const result = await client.send(command)
-   console.log(result);
+   const command = new PutObjectCommand(uploadParams)//describe las operaciones
+   return await client.send(command)
+}
+
+export async function getFiles(){
+  const command = new ListObjectsCommand({
+    Bucket: AWS_BUCKET_NAME
+  })
+  return await client.send(command)
+}
+
+export async function getFile(fileName){
+  const command = new GetObjectCommand({
+    Bucket: AWS_BUCKET_NAME,
+    Key: fileName
+  })
+  return await client.send(command)
 }
